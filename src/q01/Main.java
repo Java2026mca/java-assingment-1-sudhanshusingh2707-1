2ac7abb58f8ef43ee90e587c04ee7777bbe80dfe
sudhanshusingh2707 <yadavsudhanshuyaduvanshi@gmail.com>
2026-04-06 21:11:57 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -6,33 +6,35 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
+        
+            for (int j = i; j < n; j++) {
                 System.out.print(" ");
             }
 
+
             for (int j = 1; j <= 2 * i - 1; j++) {
-                if (j == 1 || j == 2 * i - 1) {
+                if (j == 1 || j == 2 * i - 1)
                     System.out.print("*");
-                } else {
+                else
                     System.out.print(" ");
-                }
             }
-            System.out.println();
+            if (i < n) System.out.println();
         }
 
+
         for (int i = n - 1; i >= 1; i--) {
-            for (int j = 1; j <= n - i; j++) {
+            System.out.println();
+            
+            for (int j = i; j < n; j++) {
                 System.out.print(" ");
             }
 
             for (int j = 1; j <= 2 * i - 1; j++) {
-                if (j == 1 || j == 2 * i - 1) {
+                if (j == 1 || j == 2 * i - 1)
                     System.out.print("*");
-                } else {
+                else
                     System.out.print(" ");
-                }
             }
-            System.out.println();
         }
     }
 }
