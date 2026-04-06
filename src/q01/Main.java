65e8ad8a9708ab2e780f1a69bd9954d6e1ec94bb
sudhanshusingh2707 <yadavsudhanshuyaduvanshi@gmail.com>
2026-04-06 21:18:09 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -5,36 +5,34 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+        // upper
         for (int i = 1; i <= n; i++) {
-        
-            for (int j = i; j < n; j++) {
-                System.out.print(" ");
+            for (int j = i; j < n; j++) System.out.print(" ");
+
+            if (i == 1) {
+                System.out.print("*");
+            } else {
+                System.out.print("*");
+                for (int j = 1; j <= 2*i-3; j++) System.out.print(" ");
+                System.out.print("*");
             }
 
-
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                if (j == 1 || j == 2 * i - 1)
-                    System.out.print("*");
-                else
-                    System.out.print(" ");
-            }
-            if (i < n) System.out.println();
+            System.out.println();
         }
 
+        // lower
+        for (int i = n-1; i >= 1; i--) {
+            for (int j = i; j < n; j++) System.out.print(" ");
 
-        for (int i = n - 1; i >= 1; i--) {
-            System.out.println();
-            
-            for (int j = i; j < n; j++) {
-                System.out.print(" ");
+            if (i == 1) {
+                System.out.print("*");
+            } else {
+                System.out.print("*");
+                for (int j = 1; j <= 2*i-3; j++) System.out.print(" ");
+                System.out.print("*");
             }
 
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                if (j == 1 || j == 2 * i - 1)
-                    System.out.print("*");
-                else
-                    System.out.print(" ");
-            }
+            if (i > 1) System.out.println();
         }
     }
 }
