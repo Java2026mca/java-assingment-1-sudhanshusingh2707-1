ce2e61804257ac7a6a9daa9cffb07ba49930becc
sudhanshusingh2707 <yadavsudhanshuyaduvanshi@gmail.com>
2026-04-07 15:44:46 +0530

    Update Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -4,58 +4,70 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[][] arr = new int[n][n];
 
-        int[][] matrix = new int[n][n];
+int top = 0, bottom = n - 1;
+int left = 0, right = n - 1;
+int num = 1;
 
-        int top = 0, bottom = n - 1;
-        int left = 0, right = n - 1;
-        int num = 1;
+// Fill spiral
+while (top <= bottom && left <= right) {
 
-        while (top <= bottom && left <= right) {
+    // left → right
+    for (int i = left; i <= right; i++) {
+        arr[top][i] = num++;
+    }
+    top++;
 
-            for (int i = left; i <= right; i++) {
-                matrix[top][i] = num++;
-            }
-            top++;
+    // top → bottom
+    for (int i = top; i <= bottom; i++) {
+        arr[i][right] = num++;
+    }
+    right--;
 
-            for (int i = top; i <= bottom; i++) {
-                matrix[i][right] = num++;
-            }
-            right--;
-
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    matrix[bottom][i] = num++;
-                }
-                bottom--;
-            }
-
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    matrix[i][left] = num++;
-                }
-                left++;
-            }
+    // right → left
+    if (top <= bottom) {
+        for (int i = right; i >= left; i--) {
+            arr[bottom][i] = num++;
         }
+        bottom--;
+    }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(matrix[i][j]);
-                if (j < n - 1) System.out.print(" ");
-            }
-            System.out.println();
+    // bottom → top
+    if (left <= right) {
+        for (int i = bottom; i >= top; i--) {
+            arr[i][left] = num++;
         }
+        left++;
+    }
+}
 
-        int diagonalSum = 0;
-        for (int i = 0; i < n; i++) {
-            diagonalSum += matrix[i][i];
-            diagonalSum += matrix[i][n - 1 - i];
-        }
+// Print matrix
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        System.out.print(arr[i][j]);
+        if (j < n - 1) System.out.print(" ");
+    }
+    System.out.println();
+}
 
-        if (n % 2 == 1) {
-            diagonalSum -= matrix[n / 2][n / 2];
-        }
+// Diagonal sum
+int sum = 0;
+for (int i = 0; i < n; i++) {
+    sum += arr[i][i];
+}
 
-        System.out.println("Diagonal: " + diagonalSum);
+System.out.println("Diagonal: " + sum);
+        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
+        //       Print each row with values separated by single space
+        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
+        //
+        // Input: 3
+        // Output:
+        // 1 2 3
+        // 8 9 4
+        // 7 6 5
+        // Diagonal: 15
+
     }
 }
