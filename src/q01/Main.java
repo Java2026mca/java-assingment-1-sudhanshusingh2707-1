288274b6577264316e5dab156ebaf2055cea1a37
sudhanshusingh2707 <yadavsudhanshuyaduvanshi@gmail.com>
2026-04-07 16:06:08 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -4,30 +4,48 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-    int num = 1;
-    for (int j = 0; j <= i; j++) {
-        System.out.print(num);
-        
-        if (j < i) {
-            System.out.print(" ");
-        }
-        
-        num = num * (i - j) / (j + 1);
-    }
-    System.out.println();
-}
-        // TODO: Print Pascal's Triangle for n rows
-        //       Values on each row separated by single space
-        //       No leading or trailing spaces
-        //
-        // Input: 5
-        // Output:
-        // 1
-        // 1 1
-        // 1 2 1
-        // 1 3 3 1
-        // 1 4 6 4 1
 
+        // TODO: Print a hollow diamond pattern of half-height n using '*'
+        //
+        // Rules:
+        //   - Only border cells have '*', inside is spaces
+        //   - No trailing spaces on any line
+        //
+        // Input: 4
+        // Output:
+        //    *
+        //   * *
+        //  *   *
+        // *     *
+        //  *   *
+        //   * *
+        //    *
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            if (i > 1) {
+                for (int j = 1; j <= 2 * (i - 1) - 1; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            if (i > 1) {
+                for (int j = 1; j <= 2 * (i - 1) - 1; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
