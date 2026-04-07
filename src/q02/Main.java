fca1ad19bf9fb7be884c70bce180fd3497b07b81
sudhanshusingh2707 <yadavsudhanshuyaduvanshi@gmail.com>
2026-04-07 15:41:44 +0530

    Update Main.java

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -4,23 +4,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         for (int i = 0; i < n; i++) {
-            int number = 1;
-
-            for (int j = 0; j <= i; j++) {
-                System.out.print(number);
-
-                if (j < i) {
-                    System.out.print(" ");
-                }
-
-                number = number * (i - j) / (j + 1);
-            }
-
-            System.out.println();
+    int num = 1;
+    for (int j = 0; j <= i; j++) {
+        System.out.print(num);
+        
+        if (j < i) {
+            System.out.print(" ");
         }
+        
+        num = num * (i - j) / (j + 1);
+    }
+    System.out.println();
+}
+        // TODO: Print Pascal's Triangle for n rows
+        //       Values on each row separated by single space
+        //       No leading or trailing spaces
+        //
+        // Input: 5
+        // Output:
+        // 1
+        // 1 1
+        // 1 2 1
+        // 1 3 3 1
+        // 1 4 6 4 1
 
-        sc.close();
     }
 }
