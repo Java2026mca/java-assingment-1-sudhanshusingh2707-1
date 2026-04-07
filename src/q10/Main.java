29ca1184d74c21bb1f6102f6dec7e369daffcc32
sudhanshusingh2707 <yadavsudhanshuyaduvanshi@gmail.com>
2026-04-07 15:56:39 +0530

    Update Main.java

--- src/q10/Main.java
+++ src/q10/Main.java
@@ -3,49 +3,44 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         String line = sc.nextLine();
-        String[] tokens = line.split(" ");
 
-        Stack<Integer> stack = new Stack<>();
+        String[] tokens = line.trim().split(" ");
+        int[] stack = new int[tokens.length];
+        int top = -1;
 
         for (String token : tokens) {
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
 
-            // If operator
-            if (token.equals("+") || token.equals("-") || 
-                token.equals("*") || token.equals("/")) {
-
-                int b = stack.pop();
-                int a = stack.pop();
-
+                int b = stack[top--];
+                int a = stack[top--];
                 int result = 0;
 
-                switch (token) {
-                    case "+":
-                        result = a + b;
-                        break;
-                    case "-":
-                        result = a - b;
-                        break;
-                    case "*":
-                        result = a * b;
-                        break;
-                    case "/":
-                        result = a / b;
-                        break;
-                }
+                if (token.equals("+")) result = a + b;
+                else if (token.equals("-")) result = a - b;
+                else if (token.equals("*")) result = a * b;
+                else if (token.equals("/")) result = a / b;
 
-                stack.push(result);
+                stack[++top] = result;
 
             } else {
-                // Operand
-                stack.push(Integer.parseInt(token));
+                int num = Integer.parseInt(token);
+                stack[++top] = num;
             }
         }
 
-        // Final result
-        System.out.println(stack.pop());
+        System.out.println(stack[top]);
 
-        sc.close();
+        // TODO: Evaluate a postfix (Reverse Polish Notation) expression
+        //       Operands are non-negative integers, operators are: + - * /
+        //       Use a stack to evaluate
+        //       Input: single line, tokens separated by spaces
+        //       Output: integer result
+        //
+        // Input: 5 1 2 + 4 * + 3 -
+        // Output: 14
+        //
+        // Explanation: 5 + ((1+2)*4) - 3 = 5 + 12 - 3 = 14
+
     }
 }
