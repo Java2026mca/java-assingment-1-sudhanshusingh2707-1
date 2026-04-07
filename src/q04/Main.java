5a6fee2cac509de4780fefc9083569a1f38d3963
sudhanshusingh2707 <yadavsudhanshuyaduvanshi@gmail.com>
2026-04-07 14:38:51 +0530

    Update Main.java

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -5,21 +5,51 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Read n integers. For each number print:
-        //   "Prime"     if it is prime
-        //   "Perfect"   if it is a perfect number (sum of proper divisors == itself, e.g. 6=1+2+3)
-        //   "Both"      if it is both (there are none < 100, but handle it)
-        //   "Neither"   otherwise
-        //
-        // Input:
-        // 4
-        // 6 13 8 28
-        //
-        // Output:
-        // Perfect
-        // Prime
-        // Neither
-        // Perfect
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
 
+            boolean isPrime = checkPrime(num);
+            boolean isPerfect = checkPerfect(num);
+
+            if (isPrime && isPerfect) {
+                System.out.println("Both");
+            } else if (isPrime) {
+                System.out.println("Prime");
+            } else if (isPerfect) {
+                System.out.println("Perfect");
+            } else {
+                System.out.println("Neither");
+            }
+        }
+
+        sc.close();
+    }
+
+    // Check Prime
+    public static boolean checkPrime(int num) {
+        if (num <= 1) return false;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    // Check Perfect Number
+    public static boolean checkPerfect(int num) {
+        if (num <= 1) return false;
+
+        int sum = 1;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i != num / i) {
+                    sum += num / i;
+                }
+            }
+        }
+
+        return sum == num;
     }
 }
